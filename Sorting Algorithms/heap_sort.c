74f6d95deb3b8c15cf9ef51c186a47acf933014c
rumeluddin <rumeluddin480@gmail.com>
2026-08-15 00:15:13 +0600

    Create heap_sort.c

---
+++ Sorting Algorithms/heap_sort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void heapify(int arr[], int size, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < size && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, size, largest);
+    }
+}
+
+void heap_sort(int arr[], int size) {
+    for (int i = size / 2 - 1; i >= 0; i--) {
+        heapify(arr, size, i);
+    }
+
+    for (int i = size - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array: ");
+    print_array(arr, size);
+
+    heap_sort(arr, size);
+
+    printf("Sorted Array  : ");
+    print_array(arr, size);
+
+    return 0;
+}
