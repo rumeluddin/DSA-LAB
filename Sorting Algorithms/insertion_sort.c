2d702e7af1da5a8e8dceafbf812b8e777f9fb484
rumeluddin <rumeluddin480@gmail.com>
2026-08-15 00:14:12 +0600

    Create insertion_sort.c

---
+++ Sorting Algorithms/insertion_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void insertion_sort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array: ");
+    print_array(arr, size);
+
+    insertion_sort(arr, size);
+
+    printf("Sorted Array  : ");
+    print_array(arr, size);
+
+    return 0;
+}
