31d10432f846374391f698db66aee4ca88e0c9d9
rumeluddin <rumeluddin480@gmail.com>
2026-08-15 12:37:36 +0600

    Create string_concatenation.c

---
+++ Linear Data Structures/string_concatenation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str1[50] = "Hello, ";
+    char str2[] = "World!";
+
+    strcat(str1, str2);
+
+    printf("Concatenated String: %s\n", str1);
+
+    return 0;
+}
